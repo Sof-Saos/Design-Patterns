74db97458e9a02b1bb88328a492963c5834f310a
Sof <74073693+Sof-Saos@users.noreply.github.com>
2023-09-01 07:04:13 -0500

    Delete Creational Patterns/Factory/Problema directory

--- Creational Patterns/Factory/Problema/RickC137.java
+++
@@ -1,8 +0,0 @@
-package SolucionSinPatron;
-
-public class RickC137 extends Rick{
-    @Override
-    public void mostrar() {
-        System.out.println("Soy el Rick C137");
-    }
-}
--- Creational Patterns/Factory/Problema/RickVaquero.java
+++
@@ -1,9 +0,0 @@
-package SolucionSinPatron;
-
-public class RickVaquero extends Rick{
-
-    @Override
-    public void mostrar() {
-        System.out.println("Soy el Rick Vaquero");
-    }
-}
