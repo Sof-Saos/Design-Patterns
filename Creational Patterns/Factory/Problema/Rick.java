a7a6343c2bc9ac212814fe15a3063f4babab4dd8
Sof <74073693+Sof-Saos@users.noreply.github.com>
2023-08-31 21:05:46 -0500

    Add files via upload

--- Creational Patterns/Factory/Problema/Rick.java
+++ Creational Patterns/Factory/Problema/Rick.java
@@ -1,1 +1,8 @@
+package SolucionSinPatron;
 
+public abstract class Rick { //Nos aseguramos que nadie la pueda instanciar, haciendola abstracta
+    public abstract void mostrar(); //esta clase debería ser abstracta dado que ese metodo no se implementa
+    public void teletransportarse(String dimension){ System.out.println("Teletransportandose a la dimensión: " + dimension); }
+    public void llamarMorty(){ System.out.println("Hey Morty, ven acá!"); }
+
+}
---
+++ Creational Patterns/Factory/Problema/RickC137.java
@@ -0,0 +1,8 @@
+package SolucionSinPatron;
+
+public class RickC137 extends Rick{
+    @Override
+    public void mostrar() {
+        System.out.println("Soy el Rick C137");
+    }
+}
